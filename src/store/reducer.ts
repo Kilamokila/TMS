6fd6dee41683ae5e24818496b03e2e275161a44b
Даниил Лукин <d.lukin@commit.tech>
2025-05-21 11:22:04 +0400

    feat(store): add notification reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,10 @@
 import { apiSlice } from '@services/api/rtkQuery/apiSlice';
 
 import { ROLES_REDUCER_NAME, reducer as rolesReducer } from './reducers/roles';
+import { notificationSlice } from './slices/notificationSlice';
 
 export const reducer = {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [ROLES_REDUCER_NAME]: rolesReducer,
+    [notificationSlice.reducerPath]: notificationSlice.reducer,
 };
